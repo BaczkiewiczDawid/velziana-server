7217dcf554d99f4bf9d09ff0a03a681d601a11c5
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2022-07-09 12:34:13 +0200

    handling user reservation

--- app.js
+++ app.js
@@ -6,30 +6,46 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const db = mysql.createPool({
-    host: process.env.DB_SERVER,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
+  host: process.env.DB_SERVER,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/reservation', (req, res) => {
-    const getReservations = "SELECT * FROM velziana_reservations WHERE date = 1 AND time = '10:00-12:00'";
+app.post("/reservation", (req, res) => {
+  const data = req.body.data;
 
-    db.query(getReservations, (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(result)
-        }
-    })
-})
+  const checkIsAvailable = `SELECT * FROM velziana_reservations WHERE date='${data.date}' AND time='${data.time}'`;
 
-console.log('Server running...')
+  const newReservation = `INSERT INTO velziana_reservations VALUES(null, '${data.date}', '${data.time}', 2)`;
+
+  db.query(checkIsAvailable, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result.length);
+      if (result.length === 0) {
+        db.query(newReservation, (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.send(result);
+            console.log(result);
+          }
+        });
+      } else {
+        res.send("Already reserved");
+      }
+    }
+  });
+});
+
+console.log("Server running...");
 
 const port = process.env.PORT || 5000;
 
-app.listen(port)
+app.listen(port);
